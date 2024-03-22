ecce564de84e7ea7a62b675f9bc95f50a1b54ce3
karina vergara <a01663881@tec.mx>
2024-03-22 15:46:52 -0600

    se agrego Contar y desplegar el numero de taps Detectar cuando todos los cuadros se han destapado

--- memory.py
+++ memory.py
@@ -9,16 +9,15 @@ Exercises:
 5. Use letters instead of tiles.
 """
 
-from random import *
 from turtle import *
-
+from random import shuffle
 from freegames import path
 
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
 hide = [True] * 64
-
+tap_count = 0
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -32,29 +31,30 @@ def square(x, y):
         left(90)
     end_fill()
 
-
 def index(x, y):
     """Convert (x, y) coordinates to tiles index."""
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
-
 
 def xy(count):
     """Convert tiles count to (x, y) coordinates."""
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
-
 def tap(x, y):
     """Update mark and hidden tiles based on tap."""
+    global tap_count
     spot = index(x, y)
     mark = state['mark']
-
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
     else:
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
-
+        tap_count += 1  # Increment the tap count
+        draw_taps()  # Draw the tap count on the screen
+        if all(not h for h in hide):  # Check if all tiles are revealed
+            onscreenclick(None)  # Disable further taps
+            draw_winning_message()  # Show winning message
 
 def draw():
     """Draw image and tiles."""
@@ -69,7 +69,6 @@ def draw():
             square(x, y)
 
     mark = state['mark']
-
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
@@ -78,8 +77,18 @@ def draw():
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
     update()
-    ontimer(draw, 100)
 
+def draw_taps():
+    """Draw the number of taps on the screen."""
+    goto(200, -220)
+    color('black')
+    write(f'Taps: {tap_count}', font=('Arial', 16, 'normal'))
+
+def draw_winning_message():
+    """Display winning message."""
+    goto(-100, 0)
+    color('green')
+    write('All tiles revealed!', font=('Arial', 30, 'bold'))
 
 shuffle(tiles)
 setup(420, 420, 370, 0)
@@ -89,3 +98,4 @@ tracer(False)
 onscreenclick(tap)
 draw()
 done()
+
